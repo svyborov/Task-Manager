3b47a00c17642457b2c93cc987e0c698ebfe8980
svyborov <sergey-vyborov@yandex.ru>
2019-03-02 22:17:06 +0300

    make better

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
