04d8d1b4c47878aed5e156df84ea91b29dbb2f11
svyborov <sergey-vyborov@yandex.ru>
2019-03-17 10:56:44 +0300

    rewrite eslintignore

--- routes/sessions.js
+++ routes/sessions.js
@@ -23,7 +23,7 @@ export default (router) => {
       ctx.flashMessage.notice = 'email or password were wrong';
       ctx.render('sessions/new', { f: buildFormObj({ email }) });
     })
-    .get('delsession', '/delsession', (ctx) => {
+    .delete('delsession', '/delsession', (ctx) => {
       ctx.session = {};
       ctx.redirect(router.url('root'));
     });
